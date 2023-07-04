20b7a6145a5c8b82b6d7695822920e64de6b183e
Kovanto <kovaljski@gmail.com>
2023-07-04 17:44:27 +0300

    added new StudentController

--- first-project/src/main/java/ee/jupako/firstproject/controller/StudentController.java
+++ first-project/src/main/java/ee/jupako/firstproject/controller/StudentController.java
@@ -3,9 +3,11 @@ package ee.jupako.firstproject.controller;
 import ee.jupako.firstproject.domain.student.Student;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
     @PostMapping
     public String save(@RequestBody Student student) {
